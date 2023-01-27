03a685841c9d60e5597ff7ab7889b4441f6f1de6
Eli Terry <e.terry1135@gmail.com>
2023-01-26 23:47:08 -0500

    Updates

---
+++ precache-manifest.75f244276d11db2a68aef9e40530d431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f784aa889187ae46a22b",
+    "url": "/ToDoList/static/css/main.cd8cf0ae.chunk.css"
+  },
+  {
+    "revision": "f784aa889187ae46a22b",
+    "url": "/ToDoList/static/js/main.f784aa88.chunk.js"
+  },
+  {
+    "revision": "d0acc314c0a6c805f44b",
+    "url": "/ToDoList/static/js/1.d0acc314.chunk.js"
+  },
+  {
+    "revision": "3fc0c5074fb89fa998f6",
+    "url": "/ToDoList/static/js/2.3fc0c507.chunk.js"
+  },
+  {
+    "revision": "a5e1f69653c7cdbf9f18",
+    "url": "/ToDoList/static/js/runtime~main.a5e1f696.js"
+  },
+  {
+    "revision": "43f3073e56dce3f352040717597a9839",
+    "url": "/ToDoList/index.html"
+  }
+];
